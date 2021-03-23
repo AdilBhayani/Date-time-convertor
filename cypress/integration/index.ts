bb80ff945e733024ad3914b60d2003afb5931f56
Adil Bhayani <abhayani@atlassian.com>
2021-03-23 23:57:41 +1100

    TS Cypress tests

--- cypress/integration/index.ts
+++ cypress/integration/index.ts
@@ -1,14 +1,14 @@
 describe("<DateConvertor/>", () => {
-  it("convert the time to Auckland time", async () => {
-    cy.clock(new Date(2020, 6, 24, 22, 19, '00'), ["Date"]);
-    cy.visit("http://localhost:8080/example");
+  it("convert the time to Auckland time", () => {
+    const myString: string = "http://localhost:8080/example";
+    cy.clock(new Date(2020, 6, 24, 22, 19, 0), ["Date"]);
+    cy.visit(myString);
     cy.get('[data-testid="timezone-select"]').click();
     cy.get('[data-testid="timezone-select"]').type(`Auckland{enter}`);
 
     cy.get('[data-testid="timezones"')
       .should("be.visible")
       .contains("America/New_York --> Pacific/Auckland");
-
 
     cy.get('[data-testid="time-conversion"]')
       .should("be.visible")
